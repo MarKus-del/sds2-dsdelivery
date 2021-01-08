ed134742b432bf0df308f9015a63445ce86b6cf4
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-01-08 11:32:07 -0300

    POST /orders

--- backend/src/main/java/com/markusdel/dsdelivery/controllers/OrderController.java
+++ backend/src/main/java/com/markusdel/dsdelivery/controllers/OrderController.java
@@ -6,10 +6,10 @@ import com.markusdel.dsdelivery.services.OrderService;
 import com.markusdel.dsdelivery.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -23,6 +23,14 @@ public class OrderController {
     public ResponseEntity<List<OrderDTO>> findAll() {
         List<OrderDTO> list = services.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto) {
+        dto = services.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 
 }

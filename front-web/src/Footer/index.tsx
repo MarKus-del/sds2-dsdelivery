645121eb38fd2f302a6693382edd72eec287ae4c
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-01-10 10:33:38 -0300

    arrumando links

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -10,13 +10,13 @@ function Footer() {
     <footer className="main-footer">
       App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
       <div className="footer-icons">
-        <a href="https://www.youtube.com/channel/UC3twHmWQwtqEO7u-gB_2f7g" target="_blank" >
+        <a href="https://www.youtube.com/channel/UC3twHmWQwtqEO7u-gB_2f7g" target="_blank" rel="noreferrer">
           <Youtube />
         </a>
-        <a href="https://www.linkedin.com/school/devsuperior" target="_blank" >
+        <a href="https://www.linkedin.com/school/devsuperior" target="_blank" rel="noreferrer">
           <Linkedin />
         </a>
-        <a href="https://www.instagram.com/devsuperior.ig" target="_blank" >
+        <a href="https://www.instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">
           <Instagram />
         </a>
     
